792f3ec84f8414b5263f442e3abe9a1e93b98e30
Vasileios Selamis <vasileios.selamis@gmail.com>
2019-11-10 01:00:03 +0100

    clock and load

--- README.md
+++ README.md
@@ -12,11 +12,12 @@ __Software__
 
 *...
 
+__Installation__
 
 __Resources__
 
 Links of Projects that inpired me / gave me ideas / helped me get the project done.
 
 1. https://andyfelong.com/2016/09/r-pi-clock-radio-zeroed/
-2. https://medium.com/crowdbotics/build-your-own-radio-streaming-app-with-howler-js-637f929decc0
-3. https://github.com/ct-Open-Source/ct-Raspi-Radiowecker
+1. https://medium.com/crowdbotics/build-your-own-radio-streaming-app-with-howler-js-637f929decc0
+1. https://github.com/ct-Open-Source/ct-Raspi-Radiowecker
--- clock/script.js
+++ clock/script.js
@@ -9,21 +9,19 @@ Andy Felong
 // can't use "const" in IE -- so use "var"
 var CLOCK_CHECK_SECS = 20 * 1000; // 20 seconds
 var TEMP_CHECK_MINS = 10 * 60000; // 10 minutes
-var WU_URL = "http://api.wunderground.com/api/0123456789abcdef/geolookup/conditions/q/**STATE**-like-CA/Some_City.json";
-var TEMP_UNITS = "temp_f"; // WU temperature units: Fahrenheit ("temp_f") or Celcius ("temp_c")
+var WU_URL = "https://api.openweathermap.org/data/2.5/weather?id=6949518&appid=a2ff0a2999da9e41d1c0a3e125807cb1"
+var TEMP_UNITS = "temp"; // WU temperature units: Kelvin // x − 273.15 --> Celcius
 
 
 function getTime() {
     var today = new Date();
     var hrs = today.getHours();
-    if (hrs > 12) { // use 12-hour clock, not 24
-        hrs -= 12;
-    } else if (hrs === 0) {
-        hrs = 12;
-    }
     var mins = today.getMinutes();
     if (mins < 10) {
         mins = "0" + mins; // will convert num to string but that's OK
+    }
+    if (hrs < 10) {
+        hrs = "0" + hrs;
     }
     document.getElementById('time_txt').innerHTML = hrs + ":" + mins; // update time div
     var t = setTimeout(getTime, CLOCK_CHECK_SECS);
@@ -34,18 +32,24 @@ function getTime() {
 // <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 function getTemp() {
-    jQuery(document).ready(function($) {
-        $.ajax({
-            url: WU_URL, // Weather Underground URL with key and location
-            dataType: "jsonp", // cross-domain JSON request
-            success: function(parsed_json) {
-                var location = parsed_json['location']['city']; // could use city string if desired
-                var temp_f = parsed_json['current_observation'][TEMP_UNITS]; // temperature of city passed in URL
-                temp_f = Math.round(temp_f); // round temp to nearest degree
-                var theTemp = temp_f + "&deg;";
-                document.getElementById('temp_txt').innerHTML = theTemp; // update temperature div
-            }
-        });
-    });
-    var tmp = setTimeout(getTemp, TEMP_CHECK_MINS);
+    // dummy variable
+    var theTemp = "25 " + "&#8451";
+    document.getElementById('temp_txt').innerHTML = theTemp; // update temperature div
 }
+
+var tmp = setTimeout(getTemp, TEMP_CHECK_MINS);
+
+//################################### Weather
+// Innsbruck{
+//     "id": 6949518,
+//     "name": "Innsbruck (Innenstadt)",
+//     "country": "AT",
+//     "coord": {
+//       "lon": 11.3928,
+//       "lat": 47.2672
+//     }
+
+// Example Call : http://api.openweathermap.org/data/2.5/forecast?id=6949518&APPID={APIKEY}
+//https://api.openweathermap.org/data/2.5/weather?id=6949518&appid=a2ff0a2999da9e41d1c0a3e125807cb1
+
+//https://www.internet-radio.com/player/?mount=http://38.96.148.18:8494/listen.pls&title=KSQM%20FM%2091.5&website=http://ksqmf.com
