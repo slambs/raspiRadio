637cce8053da77f807896775f504a94f7d8d5b72
slambs <vasse@Vasileioss-MacBook-Air.local>
2020-12-16 01:08:31 +0100

    small code changes same functionality

--- clock/script.js
+++ clock/script.js
@@ -1,6 +1,4 @@
-
 // Ids --> ibk = 6949518// schlitters;
-// can't use "const" in IE -- so use "var"
 var CLOCK_CHECK_SECS = 20 * 1000; // 20 seconds
 var TEMP_CHECK_MINS = 10 * 60000; // 10 minutes
 var WU_URL = "https://api.openweathermap.org/data/2.5/weather?id=2765888&appid=a2ff0a2999da9e41d1c0a3e125807cb1"
@@ -9,28 +7,25 @@ var TEMP_UNITS = "temp"; // WU temperature units: Kelvin // x − 273.15 --> Cel
 
 function getTime() {
     var today = new Date();
-    var hrs = today.getHours();
-    var mins = today.getMinutes();
-    if (mins < 10) {
-        mins = "0" + mins; // will convert num to string but that's OK
-    }
-    if (hrs < 10) {
-        hrs = "0" + hrs;
-    }
-    document.getElementById('time_txt').innerHTML = hrs + ":" + mins; // update time div
+    var timeString = today.toLocaleString('de-DE', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false
+    })
+    document.getElementById('time_txt').innerHTML = timeString; // update time div
     var t = setTimeout(getTime, CLOCK_CHECK_SECS);
-    return hrs + ":" + mins;
+    return timeString;
 }
 
 
 
 
 function getTemp() {
-    jQuery(document).ready(function ($) {
+    jQuery(document).ready(function($) {
         $.ajax({
             url: WU_URL, // Weather Underground URL with key and location
             dataType: "jsonp", // cross-domain JSON request
-            success: function (parsed_json) {
+            success: function(parsed_json) {
                 console.log(parsed_json.main.temp);
                 console.log(parsed_json.name);
                 var CityName = parsed_json.name;
@@ -54,9 +49,9 @@ function timeSnap() {
 
 function insideTempAndMoist() {
     jQuery.get('tempIn.txt', function(data) {
-        console.log(data);//process text file line by line
+        console.log(data); //process text file line by line
         document.getElementById('temp_in').innerText = data;
-     });
-     
-    setTimeout(insideTempAndMoist,20000);
+    });
+
+    setTimeout(insideTempAndMoist, 20000);
 }
