9e5c5a3d4691c772ff39927d65037d5622a8de01
slambs <vasileios.selamis@gmail.com>
2021-05-11 00:05:21 +0200

    added redirect after brightness change #4

--- clock/clock.js
+++ clock/clock.js
@@ -21,11 +21,9 @@ app.get('/setBrightness/:value', (req, res) => {
     })
     .then(newBrightnessValue => {
       res.send(
-        `  <script type="text/javascript">
-            setTimeout(()=>{window.location.href = "./index.html"}, 3000);
-           </script
-        <h1> Screen brightness is now: ${newBrightnessValue}<br><a href="index.html">Back to home page</a></h1>`
+        `<h1> Screen brightness is now: ${newBrightnessValue}<br><a href="index.html">Back to home page</a></h1>`
       )
+      res.redirect('/')
     })
     .catch(err => {
       backlight.getMaxBrightness().then(maxBrightnessValue => {
